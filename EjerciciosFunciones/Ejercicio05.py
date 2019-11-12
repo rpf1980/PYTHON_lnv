51008e3993420dcd4ff985aa0aa36bbcdd43204f
rpf1980 <united_ssh@outlook.com>
2019-11-12 14:00:42 +0100

    chores(): add exercise 5

---
+++ EjerciciosFunciones/Ejercicio05.py
@@ -0,0 +1,8 @@
+#Realiza una funcion llamada recortar(numero, mínimo, máximo) que reciba tres
+#parámetros. El primero es el número a recortar, el segundo es el límite inferior y el
+#tercero el límite superior. La función tendrá que cumplir lo siguiente:
+# a) Devolver el límite inferior si el número es menor que éste.
+# b) Devolver el límite superior si el número es mayor que éste.
+# c) Devolver el número sin cambios si no se supera ningún límite.
+# d) Dichos parámetros se solicitan al usuario.
+
