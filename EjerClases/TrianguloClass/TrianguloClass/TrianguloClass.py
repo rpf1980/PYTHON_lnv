89fc5ff67dfa6ae69084fb68fe098c32ab5add32
rpf1980 <rafapolo1980@gmail.com>
2019-11-19 21:36:38 +0100

    feat(msi): finish exercise TrianguloClass

--- EjerClases/TrianguloClass/TrianguloClass/TrianguloClass.py
+++ EjerClases/TrianguloClass/TrianguloClass/TrianguloClass.py
@@ -1,3 +1,22 @@
-class Triangulo():
-	lado = 0
-	ladoIgual = 0
+class Triangulo:
+	def __init__(self, lado, base, altura):
+		self.lado = lado
+		self.base = base
+		self.altura = altura
+
+	def perimetro(self):
+		return (self.lado * self.lado) / self.base
+
+	def area(self):
+		return (self.base * self.altura) / 2
+	
+	def muestra(self):
+		mostramos = "Lado: " + str(self.lado) + "\nBase: " + str(self.base) + "\nAltura: " + str(self.altura)
+		return mostramos
+			 
+tri = Triangulo(12,5,23)
+
+print("El perímetro del triángulo es = " + str(tri.perimetro()))
+print("El área del triángulo es = " + str(tri.area()))
+print("Las propiedades del objeto son:\n" + str(tri.muestra()))
+
