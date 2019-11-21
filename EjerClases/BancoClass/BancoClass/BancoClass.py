4aa9fd1c14c183d861c98f4bb050efc50f0bdbf5
rpf1980 <united_ssh@outlook.com>
2019-11-21 11:00:08 +0100

    chore(class): add comment on function retirar_saldo

--- EjerClases/BancoClass/BancoClass/BancoClass.py
+++ EjerClases/BancoClass/BancoClass/BancoClass.py
@@ -1,8 +1,8 @@
 class Cuenta:
-	numCuenta = 232454000034534000
-	dni = "23232323F"
-	saldo = 23454.45
-	interes = 4.3
+	numCuenta = 0
+	dni = ""
+	saldo = 0.0
+	interes = 0.0
 
 	def __init__(self, saldo, interes):
 		self.saldo = saldo
@@ -10,8 +10,8 @@ class Cuenta:
 
 	def actualizar_saldo(self):
 
-		self.saldo = interes / 365
-		return saldo
+		self.saldo = self.interes / 365
+		return self.saldo
 
 	def ingresar(self, ingreso):
 
@@ -20,21 +20,22 @@ class Cuenta:
 
 	def retirar(self, cantidad):
 
-		if(self.saldo < self.cantidad):
-			self.saldo -= self.cantidad
-		return self.cantidad
+		if(self.saldo > cantidad):
+			self.saldo -= cantidad
+		return self.saldo #Quieres mostrar lo que queda en cuenta o MOSTRAR la cantidad que hemos retirado ?
 
 	def mostrar_datos(self):
 		
 		cuenta = Cuenta(2300, 4)
 		
-		print(str(cuenta.numCuenta))
-		print(cuenta.dni)
-		print(str(cuenta.saldo))
-		print(str(cuenta.interes))
-		print(str(cuenta.actualizar_saldo()))
-		print(str(cuenta.ingresar(1500)))
-		print(str(cuenta.retirar(388.34)))
+		print("CC: " + str(cuenta.numCuenta))
+		print("DNI: " + cuenta.dni)
+		print("SALDO: " + str(cuenta.saldo))
+		print("INTERÉS: " + str(cuenta.interes))
+		print("SALDO ACTUALIZADO: " + str(cuenta.actualizar_saldo()))
+		print("INGRESO: " + str(cuenta.ingresar(1500)))
+		print("RETIRADA EFECTIVO: " + str(cuenta.retirar(388.34)))
 
 
-print(mostrar_datos())
+cuenta = Cuenta(9999, 0.21)
+print(cuenta.mostrar_datos())
---
+++ POO/Ejemplo02.py
@@ -0,0 +1,13 @@
+class Ejemplo02:
+	def publico(self):
+		print("Metodo publico")
+	def __privado(self):
+		print("Metodo privado")
+
+e = Ejemplo02()
+e.publico()
+
+# Así podríamos llamar la clase 
+#e._Ejemplo02__privado()
+
+# Getter and Setter
