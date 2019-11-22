f70f751d8aacac3bf2312b9cb7c5b1114eff2413
rpf1980 <united_ssh@outlook.com>
2019-11-22 11:15:06 +0100

    chore(class): view class banco

--- EjerClases/BancoClass/BancoClass/BancoClass.py
+++ EjerClases/BancoClass/BancoClass/BancoClass.py
@@ -4,13 +4,14 @@ class Cuenta:
 	saldo = 0.0
 	interes = 0.0
 
-	def __init__(self, saldo, interes):
+	def __init__(self, dni, saldo, interes):
+		self.dni = dni
 		self.saldo = saldo
 		self.interes = interes
 
 	def actualizar_saldo(self):
-
-		self.saldo = self.interes / 365
+		
+		self.saldo = self.saldo * (1 + self.interes / 365)
 		return self.saldo
 
 	def ingresar(self, ingreso):
@@ -26,9 +27,7 @@ class Cuenta:
 		return res 
 
 	def mostrar_datos(self):
-		
-		cuenta = Cuenta(2300, 4)
-		
+				
 		print("CC: " + str(cuenta.numCuenta))
 		print("DNI: " + cuenta.dni)
 		print("SALDO: " + str(cuenta.saldo))
@@ -38,9 +37,35 @@ class Cuenta:
 		print("RETIRADA EFECTIVO: " + str(cuenta.retirar(388.34)))
 
 # PEDIMOS DATOS POR CONSOLA AL USUARIO
+print("INGRESA LOS DATOS DEL USUARIO:")
+print()
 
-print(int(input("Nº CC: ")))
-numero_cc
+numeroCuenta = int(input("CC: "))
+numeroDni = input("DNI: ")
+cantidadSaldo = float(input("SALDO: "))
+interesCuenta = float(input("INTERÉS: "))
+print()
 
-cuenta = Cuenta(9999, 0.21)
-print(cuenta.mostrar_datos())
+# Instanciamos la clase
+c = Cuenta(numeroDni, cantidadSaldo, interesCuenta)
+
+print("Test función actualizar_saldo():")
+print(c.actualizar_saldo())
+print()
+
+print("Test función ingresar():")
+print()
+cantidadIngresas = float(input("Ingrese cantidad de efectivo: "))
+print(str(c.ingresar(cantidadIngresas)))
+print()
+
+print("Test función retirar():")
+print()
+cantidadRetiras = float(input(""))
+print(c.retirar())
+print()
+
+print("Test función mostrar_datos():")
+print(c.mostrar_datos())
+print()
+
