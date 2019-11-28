b412754ecc7b20925c3cd3a05cdd9192283d6124
rpf1980 <united_ssh@outlook.com>
2019-11-28 10:06:32 +0100

    chore(class): failure exercise 1

--- Ejerc.Py_IV/Ejercicio01.py
+++ Ejerc.Py_IV/Ejercicio01.py
@@ -5,7 +5,19 @@ la función pasada para aplicar los descuentos o el IVA a los productos de la ce
 devolver el precio final de la cesta. """
 
 #Con esta función obtenemos el precio final después de ser aplicado el IVA que sea a una cantidad dada.
-def aplicamosIva(precio, porcentaje):
-    return precio + precio * porcentaje / 100
+def aplicaIvaCalculaPrecio(precio, porcentaje):
+    return precio * (porcentaje / 100)
+
+#Con esta función obtenemo precio final de un producto tras aplicarle descuento
+def descuentoProducto(precio, desc):
+    return precio - desc
 
 #Con esta función calculamos el precio de los productor de la compra ( cesta compra )
+def calculaPrecioCompra(diccionario, funCalculos):
+    resultado = 0
+    for precio, descuento in diccionario.items():
+        total = total + funCalculos(precio, descuento)
+    return resultado
+
+print("Precio de los productos aplicando IVA = " + calculaPrecioCompra({384:21, 2387:10, 12344:4}, aplicaIvaCalculaPrecio))
+print("Precio de los productos con descuento = " + calculaPrecioCompra({384:21, 2387:10, 12344:4}, descuentoProducto))
